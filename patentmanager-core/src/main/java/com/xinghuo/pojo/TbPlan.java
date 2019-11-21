c4cac5cda96dca0672e91ef1f92512045b93834c
DL-Java <2017006156@qq.com>
2019-11-21 20:04:25 +0800

    上传tb_plan

---
+++ patentmanager-core/src/main/java/com/xinghuo/pojo/TbPlan.java
@@ -0,0 +1,25 @@
+package com.xinghuo.pojo;
+
+
+
+public class TbPlan {
+    private Integer planId;
+
+    private String planContent;
+
+    public Integer getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(Integer planId) {
+        this.planId = planId;
+    }
+
+    public String getPlanContent() {
+        return planContent;
+    }
+
+    public void setPlanContent(String planContent) {
+        this.planContent = planContent;
+    }
+}
